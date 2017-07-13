40cb643162508ec8e8ae468893bdf7faa44c807d
saadkang <saadi.kang4@gmail.com>
2017-07-13 02:27:22 -0400

    adding nested dictionary

--- basicsyntax/dictdemo.py
+++ basicsyntax/dictdemo.py
@@ -19,4 +19,24 @@ emptyDictionary = {}
 # Now we can print that empty dictionary on the console: it is just be {}
 print(emptyDictionary)
 print("*"*40)
-# Adding 
+# Adding items to the dictionary
+# We can use the same empty dictionary that we have already defined
+emptyDictionary['One'] = 1
+# Now let's print it and see what gets printed
+print(emptyDictionary)
+print("*"*40)
+# We can add more items to the dictionary
+emptyDictionary['Two'] = 2
+emptyDictionary['Three'] = 3
+# Now let's print it again
+print(emptyDictionary)
+print("*"*40)
+# Now that the items are added we can do more operations with it
+# Let's define a variable and do a addition with to the second dictionary we created
+adding = emptyDictionary['Two'] + 8
+print(adding)
+print("*"*40)
+# Now we will change the entries of the dictionary as well, let's use the 'emptyDictionary' for example
+print(emptyDictionary)
+emptyDictionary['Two'] = 10
+print(emptyDictionary)
---
+++ basicsyntax/dictnested.py
@@ -0,0 +1,5 @@
+"""
+Nested Dictionary:
+d = {'k1' : {'nestk1' : 'nestvalue1', 'nestk2' : 'nestvalue2'}}
+"""
+
