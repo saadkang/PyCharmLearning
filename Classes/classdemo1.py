8c59dfb47191d33917a303fc23bdbb9a80827e67
saadkang <saadi.kang4@gmail.com>
2017-08-01 20:16:44 -0400

    class

---
+++ Classes/classdemo1.py
@@ -0,0 +1,14 @@
+"""
+Object Oriented Programming
+"""
+
+s = "This is a String"
+a = "This is another String"
+
+s.upper()
+a.lower()
+# The instructor has single quotes inside the type() method, but I'm going to try it without and see if I get the same
+# result
+print(type(a))
+print(type(s))
+# And turns out it does give me the same result, so I wonder why is he writing single quote there
